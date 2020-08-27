7f648ab96f4df2268bb412b615cb2225b9f81253
“[Marko <[markoreact@gmail.com]>
2020-08-27 13:13:28 +0200

    Updates

---
+++ precache-manifest.7b161db6094f95e0d145ac497c5e29f2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571987cf88f95232115ac7c789ddda5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cfd8006104257cda843",
+    "url": "/static/css/main.a618c031.chunk.css"
+  },
+  {
+    "revision": "67cbb6c17bb00079d966",
+    "url": "/static/js/2.45bc8cad.chunk.js"
+  },
+  {
+    "revision": "baebdd1671010f55d6a3b55cb359d81e",
+    "url": "/static/js/2.45bc8cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfd8006104257cda843",
+    "url": "/static/js/main.ca4dcb71.chunk.js"
+  },
+  {
+    "revision": "27e277ca3f130b8f1fce",
+    "url": "/static/js/runtime-main.ad540005.js"
+  },
+  {
+    "revision": "9d2ba090037811160e06c8166eeedd50",
+    "url": "/static/media/Brian.9d2ba090.png"
+  },
+  {
+    "revision": "e8e8f759ef0ad9a3fd65a2a7cb460431",
+    "url": "/static/media/code-solid.e8e8f759.svg"
+  },
+  {
+    "revision": "165deabdba03b7ab7eb0248c7dd59796",
+    "url": "/static/media/jack.165deabd.png"
+  },
+  {
+    "revision": "e3e03e197934cc82f33d1a1851e13b19",
+    "url": "/static/media/javascript.e3e03e19.png"
+  },
+  {
+    "revision": "656eb1b979be6973ba55f447aa67ed83",
+    "url": "/static/media/jim.656eb1b9.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/static/media/up-arrow-circle.d6c4e78d.svg"
+  }
+]);
